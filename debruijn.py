4d0945dbb3f316e59e6720bad20df41e86d74075
Zhijian Mei <545902915@qq.com>
2022-06-03 04:04:06 -0600

    1

--- debruijn.py
+++ debruijn.py
@@ -1,13 +1,20 @@
+import copy
+from collections import Counter
+
+
 class Node:
     """ Class Node to represent a vertex in the de bruijn graph """
+
     def __init__(self, lab):
         self.label = lab
         self.indegree = 0
         self.outdegree = 0
 
+
 class Edge:
     def __init__(self, lab):
         self.label = lab
+
 
 def read_reads(fname):
     """ Read short reads in FASTA format. It is assumed that one line in the input file correspond to one read. """
@@ -19,8 +26,8 @@ def read_reads(fname):
     for line in lines:
         if line[0] != '>':
             reads = reads + [line.rstrip()]
-
     return reads
+
 
 def construct_graph(reads, k):
     """ Construct de bruijn graph from sets of short reads with k length word"""
@@ -29,9 +36,9 @@ def construct_graph(reads, k):
 
     for read in reads:
         i = 0
-        while i+k < len(read):
-            v1 = read[i:i+k]
-            v2 = read[i+1:i+k+1]
+        while i + k < len(read):
+            v1 = read[i:i + k]
+            v2 = read[i + 1:i + k + 1]
             if v1 in edges.keys():
                 vertices[v1].outdegree += 1
                 edges[v1] += [Edge(v2)]
@@ -49,34 +56,73 @@ def construct_graph(reads, k):
 
     return (vertices, edges)
 
+
+def DFS(current, E, vec, output):
+    vec.append(current)
+    if len(E[current]) == 0:
+        if vec not in output:
+            # printPath(vec)
+            output.append(copy.deepcopy(vec))
+        vec.pop()
+        return
+    for i in range(len(E[current])):
+        DFS(E[current][i].label, E, vec, output)
+
+    vec.pop()
+
+
+def printPath(vec):
+    # Print elements in the vector
+    for ele in vec:
+        print(ele, end=" ")
+    print()
+
+
 def output_contigs(g):
     """ Perform searching for Eulerian path in the graph to output genome assembly"""
     V = g[0]
     E = g[1]
     # Pick starting node (the vertex with zero in degree)
-    start = V.keys()[0]
-    for k in V.keys():
-        if V[k].indegree < V[start].indegree:
-            start = k
+    starts = []
+    for k in list(V.keys()):
+        if V[k].indegree == 0:
+            starts.append(k)
+    # start = list(V.keys())[0]
+    # for k in list(V.keys()):
+    #     if V[k].indegree < V[start].indegree:
+    #         start = k
+    E_copy = copy.deepcopy(E)
+    print('Number of kmers have no income edges: ', len(starts))
+    for start in starts:
+        contig = start
+        current = start
+        # E = copy.deepcopy(E_copy)
+        vec = []
+        output = []
+        DFS(current, E, vec, output)
+        # while len(E[current]) > 0:
+        #     # Pick the next node to be traversed
+        #     next = E[current][0]
+        #     del E[current][0]
+        #     contig += next.label[-1]
+        #     current = next.label
+        print('start=', start)
+        for o in output:
+            for i in range(1,len(o)):
+                contig+=o[i][-1]
+            print(contig, len(contig))
+        print('*' * 50)
+    quit()
 
-    contig = start
-    current = start
-    while len(E[current]) > 0:
-        # Pick the next node to be traversed (for now, at random)
-        next = E[current][0]
-        del E[current][0]
-        contig += next.label[-1]
-        current = next.label
 
-    return contig
 
 def print_graph(g):
     """ Print the information in the graph to be (somewhat) presentable """
     V = g[0]
     E = g[1]
     for k in V.keys():
-        print "name: ", V[k].label, ". indegree: ", V[k].indegree, ". outdegree: ", V[k].outdegree
-        print "Edges: "
+        print("name: ", V[k].label, ". indegree: ", V[k].indegree, ". outdegree: ", V[k].outdegree)
+        print("Edges: ")
         for e in E[k]:
-            print e.label
-        print
+            print(e.label)
+        print()
--- example.py
+++ example.py
@@ -1,16 +1,33 @@
+import os
+from collections import Counter
+
+import pandas as pd
 import debruijn as db
 
-# Main script
-fname = 'g200reads.fa'
-reads = db.read_reads(fname)
-# print reads
+sequences = []
+sequences_scores = []
+for root, dir, files in os.walk('avastin/avastin'):
+    root = root + '/'
+    for file in files:
+        filename = root + file
+        data = pd.read_csv(filename, delimiter='\t')
+        temp = data[data['Score']>0.5]
+        temp = temp[-50<temp['PPM Difference']]
+        temp = temp[temp['PPM Difference']<50]
+        temp.reset_index(inplace=True)
+        sequences.extend(temp['DENOVO'].values)
+        for i in range(len(temp)):
+            sequences_scores.append([temp['DENOVO'][i],temp['Score'][i]])
+            sequences.append(temp['DENOVO'][i])
+print(len(sequences))
+sequences = Counter(sequences)
+sequences = list(sequences.keys())
 
-test = ['bcdefg', 'defghi', 'abcd']
-# g = construct_graph(test, 3)
-g = db.construct_graph(reads, 11)
+k = 5
+g = db.construct_graph(sequences, k)
 # print_graph(g)
 # for k in g.keys():
 #   print k, g[k]
 # g = construct_graph(reads)
 contig = db.output_contigs(g)
-print contig
+print(contig)
