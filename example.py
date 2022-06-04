c71b9d11da85074e2445daf5fee714062ebbf5eb
Zhijian-Mei <46204799+Zhijian-Mei@users.noreply.github.com>
2022-06-04 21:34:17 +0800

    Update example.py

--- example.py
+++ example.py
@@ -22,7 +22,7 @@ for root, dir, files in os.walk('avastin/avastin'):
 
 
 sequences = Counter(sequences)
-sequences = list(sequences.keys())[:100]
+sequences = list(sequences.keys())[:20]
 print(len(sequences))
 
 
