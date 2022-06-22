f2d5b8fbaf214e8e11fd267106e2608c27ca3e21
Zhijian Mei <545902915@qq.com>
2022-06-22 10:07:42 +0800

    1

--- example.py
+++ example.py
@@ -2,65 +2,64 @@ import os
 
 from collections import Counter
 import sys
+
 sys.setrecursionlimit(10000)
 import pandas as pd
 import test_debruijn as db
 
 
-def getScore(edge_count_table,contig,k):
+def getScore(edge_count_table, contig, k):
     score = 0
-    for i in range(len(contig)-k):
-        score +=edge_count_table[contig[i:i+k+1]]
+    for i in range(len(contig) - k):
+        score += edge_count_table[contig[i:i + k + 1]]
     return score
+
 
 sequences = []
 sequences_scores = []
-score_cut = 0.95
+score_cut = 0.5
+threshold = 3
 for root, dir, files in os.walk('avastin/avastin'):
     root = root + '/'
     for file in files:
         filename = root + file
         data = pd.read_csv(filename, delimiter='\t')
-        # temp = data
-        temp = data[data['Score']>=score_cut]
-        temp = temp[-50<temp['PPM Difference']]
-        temp = temp[temp['PPM Difference']<50]
+        temp = data[data['Score'] >= score_cut]
+        temp = temp[-50 < temp['PPM Difference']]
+        temp = temp[temp['PPM Difference'] < 50]
         temp.reset_index(inplace=True)
         sequences.extend(temp['DENOVO'].values)
-        for i in range(len(temp)):
-            sequences_scores.append([temp['DENOVO'][i],temp['Score'][i]])
-            sequences.append(temp['DENOVO'][i])
 
 
 sequences = Counter(sequences)
 sequences = list(sequences.keys())
 print(len(sequences))
 
-# sequences = ['EVQLVESGGGLVQPGGSLRLSCAASGYTFTNYGMNWVRQAPGKGLEWVGWLNTYTGEPTYAADFKRRFTFSLDTSKSTAYLQMNSLRAEDTAVYYCAKYPHYYGSSHWYFDVWGQGTLVTVSSASTKGPSVFPLAPSSKSTSGGTAALGCLVKDYFPEPVTVSWNSGALTSGVHTFPAVLQSSGLYSLSSVVTVPSSSLGTQTYLCNVNHKPSNTKVDKKVEPKSCDKTHTCPPCPAPELLGGPSVFLFPPKPKDTLMLSRTPEVTCVVVDVSHEDPEVKFNWYVDGVEVHNAKTKPREEQYNSTYRVVSVLTVLHQDWLNGKEYKCKVSNKALPAPLEKTLSKAKGQPREPQVYTLPPSREEMTKNQVSLTCLVKGFYPSDLAVEWESNGQPENNYKTTPPVLDSDGSFFLYSKLTVDKSRWQQGNVFSCSVMHEALHNHYTQKSLSLSPGK']
+# sequences = [
+# 'EVQLVESGGGLVQPGGSLRLSCAASGYTFTNYGMNWVRQAPGKGLEWVGWLNTYTGEPTYAADFKRRFTFSLDTSKSTAYLQMNSLRAEDTAVYYCAKYPHYYGSSHWYFDVWGQGTLVTVSSASTKGPSVFPLAPSSKSTSGGTAALGCLVKDYFPEPVTVSWNSGALTSGVHTFPAVLQSSGLYSLSSVVTVPSSSLGTQTYLCNVNHKPSNTKVDKKVEPKSCDKTHTCPPCPAPELLGGPSVFLFPPKPKDTLMLSRTPEVTCVVVDVSHEDPEVKFNWYVDGVEVHNAKTKPREEQYNSTYRVVSVLTVLHQDWLNGKEYKCKVSNKALPAPLEKTLSKAKGQPREPQVYTLPPSREEMTKNQVSLTCLVKGFYPSDLAVEWESNGQPENNYKTTPPVLDSDGSFFLYSKLTVDKSRWQQGNVFSCSVMHEALHNHYTQKSLSLSPGK']
 
 k_lowerlimit = 5
-k_upperlimit = 20
-for k in range(k_lowerlimit,k_upperlimit+1):
-    if k <= k_upperlimit-1:
-        g, pull_out_read,branch_kmer,already_pull_out,edge_count_table = db.construct_graph(sequences, k, threshold=2)
+k_upperlimit = 10
+for k in range(k_lowerlimit, k_upperlimit + 1):
+    if k <= k_upperlimit - 1:
+        g, pull_out_read, branch_kmer, already_pull_out, edge_count_table = db.construct_graph(sequences, k,
+                                                                                               threshold=threshold)
     else:
-        g, pull_out_read, branch_kmer,already_pull_out,edge_count_table = db.construct_graph(sequences, k, threshold=2, final=True)
+        g, pull_out_read, branch_kmer, already_pull_out, edge_count_table = db.construct_graph(sequences, k,
+                                                                                               threshold=threshold, final=True)
 
-    sequences = db.output_contigs(g,branch_kmer,already_pull_out)
-    sequences.sort(key=lambda x: getScore(edge_count_table,x,k),reverse=True)
+    sequences = db.output_contigs(g, branch_kmer, already_pull_out)
+    sequences.sort(key=lambda x: getScore(edge_count_table, x, k), reverse=True)
     if k == k_upperlimit:
-        outFile = open('avastin_{}mer_{}.fasta'.format(k,score_cut),mode='a+')
+        outFile = open('avastin_{}-{}mer_{}.fasta'.format(k_lowerlimit,k, score_cut), mode='a+')
         for i in range(len(sequences)):
-            outFile.writelines('>SEQUENCE_{}_{}mer\n{}\n'.format(i,k,sequences[i]))
+            outFile.writelines('>SEQUENCE_{}_{}mer\n{}\n'.format(i, k, sequences[i]))
         outFile.close()
-    print('max length: ',len(max(sequences,key=lambda x:len(x))))
-    print('number of output for k={}: '.format(k),len(sequences))
-    if k <= k_upperlimit-1:
+    print('max length: ', len(max(sequences, key=lambda x: len(x))))
+    print('number of output for k={}: '.format(k), len(sequences))
+    if k <= k_upperlimit - 1:
         sequences.extend(pull_out_read)
-        print('number of pull out read: ',len(pull_out_read))
-
-
-
+        print('number of pull out read: ', len(pull_out_read))
 
 
 # print('*' * 100)
---
+++ generateReport.py
@@ -0,0 +1,44 @@
+import os
+
+import pandas as pd
+from tqdm import trange
+
+from test_debruijn import read_reads
+
+def findSupportReadScore(contig,score_table):
+    score = 0
+    for read in score_table.keys():
+        if read in contig:
+            score += sequences_scores[read]
+    return score
+
+
+fname = 'avastin_5-10mer_0.5_modified'
+
+filePath='avastin/avastin'
+score_cut = 0.5
+sequences_scores = dict()
+for root, dir, files in os.walk(filePath):
+    root = root + '/'
+    for file in files:
+        filename = root + file
+        data = pd.read_csv(filename, delimiter='\t')
+        temp = data[data['Score'] >= score_cut]
+        temp = temp[-50 < temp['PPM Difference']]
+        temp = temp[temp['PPM Difference'] < 50]
+        temp.reset_index(inplace=True)
+        for i in range(len(temp)):
+            if temp['DENOVO'][i] not in sequences_scores.keys():
+                sequences_scores[temp['DENOVO'][i]] = temp['Score'][i]
+            else:
+                sequences_scores[temp['DENOVO'][i]] = temp['Score'][i] + sequences_scores[temp['DENOVO'][i]]
+
+contigs = read_reads(fname+'.fasta')
+scores = []
+
+k=10
+contigs.sort(key=lambda x:findSupportReadScore(x,score_table=sequences_scores),reverse=True)
+outFile = open(fname+'_sorted.fasta', mode='a+')
+for i in range(len(contigs)):
+    outFile.writelines('>SEQUENCE_{}_{}mer_{}\n{}\n'.format(i,k,round(findSupportReadScore(contigs[i],sequences_scores),2),contigs[i]))
+outFile.close()
