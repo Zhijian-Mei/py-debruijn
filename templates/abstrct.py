4085cca22405a015476473080f7c76e9ad511014
Zhijian Mei <545902915@qq.com>
2022-07-13 01:36:50 -0600

    1

---
+++ templates/abstrct.py
@@ -0,0 +1,22 @@
+
+def read_fasta(fname):
+    f = open(fname, 'r')
+    lines = f.readlines()
+    f.close()
+    fasta={}
+
+    for i in range(len(lines)):
+        line = lines[i]
+        if '9606' in line:
+            fasta[line] = lines[i+1]
+    return fasta
+
+fasta = read_fasta('homo_template.fasta')
+
+
+outfile = 'homo_templates_final.fasta'
+f = open(outfile,'w')
+for key in fasta.keys():
+    f.writelines(key)
+    f.writelines(fasta[key])
+f.close()
