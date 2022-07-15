15386abbcb4e8ce036be4003ef9e60113293cfd0
Zhijian Mei <545902915@qq.com>
2022-07-16 01:01:18 +0800

    Update FindTemplateGroups.py

--- FindTemplateGroups.py
+++ FindTemplateGroups.py
@@ -1,7 +1,13 @@
+import json
+import os
 import re
 from pprint import pprint
+import ast
+import numpy as np
+
 
 import pandas as pd
+from tqdm import trange
 
 from generateTemplatesBlastReport import read_fasta
 
@@ -11,6 +17,9 @@ class Template:
         self.id = template_id
         self.contigArrays = []
         self.different_position = []
+        self.letters_correctRate = {}
+        for i in range(len(self.sequence)):
+            self.letters_correctRate[i] = {}
 
 class Contig:
     def __init__(self,contig_id,contig_sequence,template_interval,contig_interval):
@@ -18,6 +27,9 @@ class Contig:
         self.id = contig_id
         self.template_interval = template_interval
         self.contig_interval = contig_interval
+        self.rates = {}
+        for i in range(len(self.sequence)):
+            self.rates[i] = 1
 
 class fillingTemplate:
     def __init__(self,template_sequence):
@@ -61,6 +73,23 @@ if __name__ == '__main__':
     template_name = 'templates/homo_template.fasta'
     froot = 'avastin_5-10mer_0.6_2'
     contig_filepath = f'{froot}/{froot}_modified_sorted.fasta'
+
+    settingFile = open(f'{froot}/setting.json', 'r')
+    setting = json.load(settingFile)
+    souceFilePath = 'avastin/avastin'
+    DF = pd.DataFrame()
+    for root, dir, files in os.walk(souceFilePath):
+        root = root + '/'
+        for file in files:
+            filename = root + file
+            data = pd.read_csv(filename, delimiter='\t')
+            temp = data[data['Score'] >= setting['score_cut']]
+            temp = temp[-50 < temp['PPM Difference']]
+            temp = temp[temp['PPM Difference'] < 50]
+            temp.reset_index(inplace=True, drop=True)
+            DF = DF.append(temp)
+    DF.reset_index(inplace=True, drop=True)
+
     df = pd.read_csv(f'{froot}/{froot}_blasthomoTemplate.m8', delimiter='\t', header=None)
     df = df[df[2] >= 80]
     df = df.sort_values(by=0)
@@ -114,9 +143,9 @@ if __name__ == '__main__':
 
     # pprint(template_contig_group)
 
-    report_path = f'{froot}/{froot}_TemplateMatchReport.txt'
-    outFile = open(report_path, 'w')
-    message = ''
+    # report_path = f'{froot}/{froot}_TemplateMatchReport.txt'
+    # outFile = open(report_path, 'w')
+    # message = ''
 
 
     for template_id in template_contig_group.keys():
@@ -137,39 +166,86 @@ if __name__ == '__main__':
             else:
                 template.contigArrays.append([contig])
 
+        reads = DF['DENOVO'].values
+        position_scores = DF['Positional Score'].values
 
-        result_sequences = []
+        for array_index in range(len(template.contigArrays)):
+            contig_array = template.contigArrays[array_index]
+            contig_array = sorted(contig_array, key=lambda x: x.template_interval[0])
+            for contig in contig_array:
+                for i in range(len(reads)):
+                    read = reads[i]
+                    if read in contig.sequence:
+                        match = re.search(read,contig.sequence)
+                        read_positional_scores = ast.literal_eval(position_scores[i])
+                        for j in range(match.start(),match.end()):
+                            contig.rates[j] = contig.rates[j] * (1-read_positional_scores[j - match.start()])
+
+        print()
+        print('*' * 500)
+        print(template.sequence)
         for contig_array in template.contigArrays:
-            contig_array = sorted(contig_array,key=lambda x:x.template_interval[0])
-            match_result = fillingTemplate(template.sequence)
-            for i in range(len(contig_array)):
-                contig = contig_array[i]
+            contig_array = sorted(contig_array, key=lambda x: x.template_interval[0])
+            for contig in contig_array:
                 if (contig.contig_interval[1] - contig.contig_interval[0]) != (contig.template_interval[1] - contig.template_interval[0]):
                     continue
-                match_result.fill_match(contig)
-            result_sequence = match_result.get_match_result()
-            if not re.search('[a-zA-Z]', result_sequence):
-                continue
-            result_sequences.append(result_sequence)
-            different_position = [index for index in range(len(template.sequence)) if template.sequence[index] != result_sequence[index] and result_sequence[index] != ' ']
-            for position in different_position:
-                if position not in template.different_position:
-                    template.different_position.append(position)
-        message += '-' * 500
-        message += '\n'
-        message += 'Template ID: {}\n'.format(template.id)
-        changed_line = list(' ' * len(template.sequence))
-        for position in template.different_position:
-            changed_line[position] = '*'
-        changed_line = ''.join(changed_line)
-        message += changed_line
-        message += '\n'
-        message += '{}\n'.format(template.sequence)
-        for result_sequence in result_sequences:
-            message += result_sequence
-            message += '\n'
+                template_points = [x for x in range(contig.template_interval[0]-1,contig.template_interval[1])]
+                contig_points = [x for x in range(contig.contig_interval[0] - 1,contig.contig_interval[1])]
+                for i in range(len(template_points)):
+                    template_point = template_points[i]
+                    contig_point = contig_points[i]
+                    current_template_position = template.letters_correctRate[template_point]
+                    current_contig_letter = contig.sequence[contig_point]
+                    if current_contig_letter not in current_template_position.keys():
+                        current_template_position[current_contig_letter] = contig.rates[contig_point]
+                    else:
+                        current_template_position[current_contig_letter] = current_template_position[current_contig_letter] * contig.rates[contig_point]
+        position_keys = list(template.letters_correctRate.keys())
+        result_sequences = []
+        for key in position_keys:
+            candidate_letters = template.letters_correctRate[key]
+            if candidate_letters == {}:
+                print(' ',end='')
+            else:
+                candidate_letters = dict(sorted(candidate_letters.items(), key=lambda item: item[1]))
+                # if len(candidate_letters)>1:
+                #     print(candidate_letters)
+                print(list(candidate_letters.keys())[0],end='')
 
 
-    outFile.write(message)
-    outFile.close()
+
+        # result_sequences = []
+        # for contig_array in template.contigArrays:
+        #     contig_array = sorted(contig_array,key=lambda x:x.template_interval[0])
+        #     match_result = fillingTemplate(template.sequence)
+        #     for i in range(len(contig_array)):
+        #         contig = contig_array[i]
+        #         if (contig.contig_interval[1] - contig.contig_interval[0]) != (contig.template_interval[1] - contig.template_interval[0]):
+        #             continue
+        #         match_result.fill_match(contig)
+        #     result_sequence = match_result.get_match_result()
+        #     if not re.search('[a-zA-Z]', result_sequence):
+        #         continue
+        #     result_sequences.append(result_sequence)
+        #     different_position = [index for index in range(len(template.sequence)) if template.sequence[index] != result_sequence[index] and result_sequence[index] != ' ']
+        #     for position in different_position:
+        #         if position not in template.different_position:
+        #             template.different_position.append(position)
+        # message += '-' * 500
+        # message += '\n'
+        # message += 'Template ID: {}\n'.format(template.id)
+        # changed_line = list(' ' * len(template.sequence))
+        # for position in template.different_position:
+        #     changed_line[position] = '*'
+        # changed_line = ''.join(changed_line)
+        # message += changed_line
+        # message += '\n'
+        # message += '{}\n'.format(template.sequence)
+        # for result_sequence in result_sequences:
+        #     message += result_sequence
+        #     message += '\n'
+
+
+    # outFile.write(message)
+    # outFile.close()
 
