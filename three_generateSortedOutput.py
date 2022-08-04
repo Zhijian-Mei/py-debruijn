c5195a4f11d3b6fde44bfae2eff255abcb510afd
Zhijian Mei <545902915@qq.com>
2022-08-04 13:21:16 +0800

    1

---
+++ three_generateSortedOutput.py
@@ -0,0 +1,48 @@
+import json
+import os
+
+import pandas as pd
+from tqdm import trange
+
+from test_debruijn import read_reads
+
+def findSupportReadScore(contig,score_table):
+    score = 0
+    for read in score_table.keys():
+        if read in contig:
+            score += score_table[read]
+    return score
+
+if __name__ == '__main__':
+    froot = 'BSA_5-10mer_0.5_2'
+    filePath='BSA/all'
+    f = open(f'{froot}/setting.json')
+    setting = json.load(f)
+    print(setting)
+
+    score_cut = setting['score_cut']
+    sequences_scores = dict()
+    for root, dir, files in os.walk(filePath):
+        root = root + '/'
+        for file in files:
+            filename = root + file
+            data = pd.read_csv(filename, delimiter='\t')
+            temp = data[data['Score'] >= score_cut]
+            temp = temp[-50 < temp['PPM Difference']]
+            temp = temp[temp['PPM Difference'] < 50]
+            temp.reset_index(inplace=True)
+            for i in range(len(temp)):
+                if temp['DENOVO'][i] not in sequences_scores.keys():
+                    sequences_scores[temp['DENOVO'][i]] = temp['Score'][i]
+                else:
+                    sequences_scores[temp['DENOVO'][i]] = temp['Score'][i] + sequences_scores[temp['DENOVO'][i]]
+
+    contigs = read_reads(f'{froot}/{froot}_modified.fasta')
+    scores = []
+
+    k=setting['k_upperlimit']
+    contigs.sort(key=lambda x:findSupportReadScore(x,score_table=sequences_scores),reverse=True)
+    outFile = open(f'{froot}/{froot}_modified_sorted.fasta', mode='a+')
+    for i in range(len(contigs)):
+        outFile.writelines('>SEQUENCE_{}_{}mer_{}\n{}\n'.format(i,k,round(findSupportReadScore(contigs[i],sequences_scores),2),contigs[i]))
+    outFile.close()
---
+++ two_generateFinalOutput.py
@@ -0,0 +1,46 @@
+import json
+
+from test_debruijn import read_reads
+
+def checkSubSequence(contig,output):
+    for o in output:
+        if contig in o:
+            return False
+    return True
+
+def checkHeadToTail(c1,contigs,k):
+    result = c1
+    for c2 in contigs:
+        if result[:k] == c2[len(c2)-k:]:
+            result = c2 + result[k:]
+        elif c2[:k] == result[len(c1) - k:]:
+            result = result + c2[k:]
+    return result
+
+froot = 'BSA_5-10mer_0.5_2'
+contigs = read_reads(f'{froot}/{froot}.fasta')
+f = open(f'{froot}/setting.json')
+setting = json.load(f)
+print(setting)
+print('max length before concat: ',len(max(contigs,key=lambda x:len(x))))
+
+temp = []
+for c1 in contigs:
+    result = checkHeadToTail(c1,contigs,3)
+    temp.append(result)
+
+
+
+output=[]
+for contig in temp:
+    if contig not in output and checkSubSequence(contig,output):
+        output.append(contig)
+
+
+print('max length after concat: ',len(max(output,key=lambda x:len(x))))
+
+k = setting['k_upperlimit']
+outFile = open(f'{froot}/{froot}_modified.fasta', mode='a+')
+for i in range(len(output)):
+    outFile.writelines('>SEQUENCE_{}_{}mer\n{}\n'.format(i,k, output[i]))
+outFile.close()
