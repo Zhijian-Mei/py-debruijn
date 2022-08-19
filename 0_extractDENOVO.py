6bd251a7c374c472d340d1937a3f2467fa2a9d7e
Zhijian Mei <545902915@qq.com>
2022-08-19 11:49:22 +0800

    Update 0_extractDENOVO.py

--- 0_extractDENOVO.py
+++ 0_extractDENOVO.py
@@ -38,6 +38,8 @@ if __name__ == '__main__':
             # temp = temp[temp['Score'] > 0]
             unused_reads=temp['DENOVO'].values
             unused_reads = list(Counter(unused_reads).keys())
+            print(unused_reads)
+            quit()
             unused_reads = [x for x in unused_reads if len(x) > k_lowerlimit]
     quit()
 
