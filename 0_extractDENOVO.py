7e6468fe4acda07feda70e13244c0a51d2967de2
Zhijian Mei <545902915@qq.com>
2022-08-19 11:46:03 +0800

    Update 0_extractDENOVO.py

--- 0_extractDENOVO.py
+++ 0_extractDENOVO.py
@@ -5,17 +5,20 @@ from collections import Counter
 
 import pandas as pd
 
+
 def get_args():
     parser = argparse.ArgumentParser()
     # start
-    parser.add_argument('-source', type=str,required=True)
-    parser.add_argument('-score', type=float,required=True)
-    parser.add_argument('-t', type=int,required=True)
-    parser.add_argument('-kl', type=int,required=True)
-    parser.add_argument('-ku', type=int,required=True)
+    parser.add_argument('-source', type=str, required=True)
+    parser.add_argument('-score', type=float, required=True)
+    parser.add_argument('-t', type=int, required=True)
+    parser.add_argument('-kl', type=int, required=True)
+    parser.add_argument('-ku', type=int, required=True)
 
     args = parser.parse_args()
     return args
+
+
 if __name__ == '__main__':
     args = get_args()
     score_cut = args.score
@@ -24,21 +27,20 @@ if __name__ == '__main__':
     k_upperlimit = args.ku
     source = f'{args.source}/{args.source}'
     froot = f'{args.source}_{k_lowerlimit}-{k_upperlimit}mer_{score_cut}_{threshold}'
-    unused_reads = []
+
     for root, dir, files in os.walk(source):
+
         root = root + '/'
         for file in files:
             filename = root + file
-            if '1111466_E' not in filename:
-                continue
+            print(filename)
+            quit()
             data = pd.read_csv(filename, delimiter='\t')
             temp = data[data['Score'] < score_cut]
-            temp = temp[temp['Score'] > 0.1]
-            unused_reads.extend(temp['DENOVO'].values)
-
-    unused_reads = list(Counter(unused_reads).keys())
-    unused_reads = [x for x in unused_reads if len(x) > k_lowerlimit]
-    print('number of unused reads: ',len(unused_reads))
+            # temp = temp[temp['Score'] > 0]
+            unused_reads=temp['DENOVO'].values
+            unused_reads = list(Counter(unused_reads).keys())
+            unused_reads = [x for x in unused_reads if len(x) > k_lowerlimit]
 
 
     df = pd.DataFrame()
@@ -50,10 +52,17 @@ if __name__ == '__main__':
     setting = {'score_cut': score_cut, 'threshold': threshold, 'k_lowerlimit': k_lowerlimit,
                'k_upperlimit': k_upperlimit}
 
-    os.mkdir(froot)
+    try:
+        os.mkdir(froot)
+    except:
+        pass
+
     with open(f'{froot}/setting.json', 'w') as fw:
         json.dump(setting, fw, indent=4)
-    df.to_csv(f'{froot}/unused_reads.tsv',sep='\t')
-    os.system(f'python PredFull/predfull.py --input {froot}/unused_reads.tsv --model PredFull/pm.h5 --output {froot}/unused_reads_prediction.mgf')
-
-
+    df.to_csv(f'{froot}/unused_reads.tsv', sep='\t')
+    os.system(
+        f'python PredFull/predfull.py --input {froot}/unused_reads.tsv --model PredFull/pm.h5 --output {froot}/unused_reads_prediction.mgf'
+    )
+    os.system(
+        f'./msSLASH/bin/bruteforce  -e 1111466_E.mgf -l unused_reads_prediction.mgf -d empty.mgf'
+    )
